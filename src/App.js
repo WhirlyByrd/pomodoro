0a6336e75bb5942428f129b33a13c36988fb9453
WhirlyByrd <kara.r.gardner@gmail.com>
2023-01-30 11:45:40 -0600

    visual components no functionality yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import Timer from './components/Timer';
+import Settings from './components/Settings';
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
+
+  const [showSettings, setShowSettings] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {showSettings ? <Settings /> : <Timer />}
+    </main>
   );
 }
 
