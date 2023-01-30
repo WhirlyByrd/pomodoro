1108b16cd9c01693cb685f2d13434f3e52a3ca78
WhirlyByrd <kara.r.gardner@gmail.com>
2023-01-30 13:56:20 -0600

    showSettings button onClick functions

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css'
 import Timer from './components/Timer';
 import Settings from './components/Settings';
 import './App.css';
@@ -6,13 +7,15 @@ import SettingsContext from './components/SettingsContext';
 
 function App() {
 
-  const [showSettings, setShowSettings] = useState(true);
+  const [showSettings, setShowSettings] = useState(false);
   const [workMinutes, setWorkMinutes] = useState(25);
   const [breakMinutes, setBreakMinutes] = useState(5);
 
   return (
     <main>
       <SettingsContext.Provider value={{
+        showSettings,
+        setShowSettings,
         workMinutes,
         breakMinutes,
         setWorkMinutes,
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,11 +4,14 @@ import 'react-circular-progressbar/dist/styles.css';
 import PlayButton from './PlayButton';
 import PauseButton from './PauseButton';
 import SettingsButton from './SettingsButton';
+import { useContext } from 'react';
+import SettingsContext from './SettingsContext';
 
 const red = '#f54e4e';
 const green = '#4aec8c'
 
 function Timer() {
+  const settingsInfo = useContext(SettingsContext);
   return (
     <div>
      <CircularProgressbar value={60} text={`60%`} styles={buildStyles({
@@ -22,7 +25,7 @@ function Timer() {
       <PauseButton />
     </div>
       <div style={{marginTop:'20px'}}>
-        <SettingsButton />
+        <SettingsButton onClick={() => settingsInfo.setShowSettings(true)}/>
         </div>    
 
     </div>
